0635ee1169d3fda46c34ed76580d4291c88c2a06
pkulkark <pooja@opencraft.com>
2020-08-17 14:24:28 +0530

    fix issue caused by xss fix in video transcript settings

--- cms/static/js/views/video/translations_editor.js
+++ cms/static/js/views/video/translations_editor.js
@@ -1,10 +1,10 @@
 define(
     [
-        'jquery', 'underscore',
+        'jquery', 'underscore', 'edx-ui-toolkit/js/utils/html-utils',
         'js/views/abstract_editor', 'js/models/uploads', 'js/views/uploads'
 
     ],
-function($, _, AbstractEditor, FileUpload, UploadDialog) {
+function($, _, HtmlUtils, AbstractEditor, FileUpload, UploadDialog) {
     'use strict';
 
     var VideoUploadDialog = UploadDialog.extend({
@@ -118,12 +118,16 @@ function($, _, AbstractEditor, FileUpload, UploadDialog) {
                     'lang': key,
                     'value': value,
                     'url': self.model.get('urlRoot') + '/' + key
-                })).prepend(dropdown.clone().val(key))[0];
+                }));
 
-                frag.appendChild($html);
+                HtmlUtils.prepend($html, HtmlUtils.HTML(dropdown.clone().val(key)));
+                frag.appendChild($html[0]);
             });
 
-            this.$el.find('ol').html([frag]);
+            HtmlUtils.setHtml(
+                this.$el.find('ol'),
+                HtmlUtils.HTML([frag])
+            );
         },
 
         addEntry: function(event) {
