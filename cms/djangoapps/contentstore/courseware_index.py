c9bba510b412c7cf5b7c1bdd12bcb10f346560c4
Olha Zamirets <olha.zamirets@raccoongang.com>
2020-10-19 18:02:19 +0300

    [JuniperOeX-151] [JuniperOeX-152] Course with visibility 'about' and 'none' present on courses page
    fix: elasticsearch error on discover new page

--- cms/djangoapps/contentstore/courseware_index.py
+++ cms/djangoapps/contentstore/courseware_index.py
@@ -587,6 +587,7 @@ class CourseAboutSearchIndexer(CoursewareSearchIndexer):
         AboutInfo("org", AboutInfo.PROPERTY, AboutInfo.FROM_COURSE_PROPERTY),
         AboutInfo("modes", AboutInfo.PROPERTY, AboutInfo.FROM_COURSE_MODE),
         AboutInfo("language", AboutInfo.PROPERTY, AboutInfo.FROM_COURSE_PROPERTY),
+        AboutInfo("catalog_visibility", AboutInfo.PROPERTY, AboutInfo.FROM_COURSE_PROPERTY),
     ]
 
     @classmethod
