0bc65143126da56a0813883e4888940e74315497
Glib Glugovskiy <glib.glugovskiy@raccoongang.com>
2020-11-05 17:30:33 +0000

    Change link paths

--- lms/djangoapps/branding/static/branding/js/constants.js
+++ lms/djangoapps/branding/static/branding/js/constants.js
@@ -2,13 +2,13 @@ const navcards = [
     {
         title: 'My Pathway',
         description: 'Select your role and courses so you can begin your 40 hours of learning.',
-        link: 'https://courses/pathway',
+        link: 'http://courses/pathway/',
         name: 'Pathway',
     },
     {
         title: 'Course Catalog',
         description: 'Search through the course catalog to see what you can learn.',
-        link: 'http://courses',
+        link: 'http://courses/',
         name: 'Courses',
     },
     {
