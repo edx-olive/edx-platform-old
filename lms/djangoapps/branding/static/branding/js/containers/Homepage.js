2a935a7b4b1e37611eaba2d2a8bb29160acb9520
Glib Glugovskiy <glib.glugovskiy@raccoongang.com>
2020-12-23 22:33:05 +0000

    Merged in amat-dev (pull request #254)
    (Sync stage) Homepage notifications and edx header
    
    * Merged in AMATX-948/notifs-context (pull request #249)
    
    [AMATX-948] Request a list of notifications from the AMAT catalogue backend
    
    * AMATX-948: Add teams/notifications context to the homepage
    
    Approved-by: Evgen Dyudyunov
    Approved-by: Glib Glugovskiy
    
    * [AMATX-994] Add header/footer layout
    
    - add 'headerOnly' condition for the homepage component;
    - make 'Footer' an independent component;
    - apply new header and footer for all edx pages;
    - `COURSE CATALOG` is highlighted when `headerOnly` prop is true
    - all homepage links changed to `http://...`
    
    **Youtrack:**
    - https://youtrack.raccoongang.com/issue/AMATX-994
    
    **Relates to:**
    - THEME MR
    
    * Merge branch 'dyudyunov/AMATX-994/add-header-footer-layout' into 'amat-dev'
    
    [AMATX-994] Add header/footer layout
    
    See merge request glib.glugovskiy/edx-platform!5
    * Merged in dyudyunov/AMATX-994/add-header-footer-layout (pull request #253)
    
    [AMATX-994] Add header/footer layout
    
    Approved-by: Olena Persianova
    Approved-by: Glib Glugovskiy
    
    * Merge branch 'amat-dev' of gitlab.raccoongang.com:glib.glugovskiy/edx-platform into amat-dev
    
    * [AMATX-975] Create layout and markup for dropdown component
    
    * Merge branch 'AMATX-975-dropdown-component' into 'amat-dev'
    
    [AMATX-975] Create layout and markup for dropdown component
    
    See merge request glib.glugovskiy/edx-platform!4
    * Merge branch 'amat-dev' of gitlab.raccoongang.com:glib.glugovskiy/edx-platform into amat-dev

--- lms/djangoapps/branding/static/branding/js/containers/Homepage.js
+++ lms/djangoapps/branding/static/branding/js/containers/Homepage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import navcards from '../constants';
-import Footer from './Footer';
 import Header from './Header';
 import ManagerDashboard from '../pages/ManagerDashboard';
 import Navcards from '../components/navcards/Navcards';
 import { UserProfileProvider } from '../context/UserProfile'
+import { TeamProvider } from '../context/Team'
 import { APIDataProvider } from '../context/APIContext'
 
 
@@ -16,29 +16,35 @@ export class Homepage extends React.Component {
 
     render() {
 
+        const headerOnly = this.props.headerOnly || false
+
         return (
             <div>
                 <APIDataProvider
                   baseAPIUrl={this.props.baseAPIUrl} awsSettings={this.props.awsSettings}
                   userId={this.props.userId} reqHeaders={this.props.reqHeaders}>
                     <UserProfileProvider>
-                        <Header pathname='/home' logoImg={this.props.amatLogo}></Header>
-                        <div className="MainHolder">
-                            <div className="OnDemandDash">
-                                <p>Looking for the ON DEMAND Dashboard?</p>
-                                <a href="/dashboard">Click here</a>
-                            </div>
-                            <div className="MainHolderWrapper">
-                                <div className="Big-Logo-Img">
-                                    <img src={this.props.amatLogo} alt="appliedx"/>
+                      <TeamProvider>
+                        <Header pathname={headerOnly ? 'http://courses/' : '/home'} logoImg={this.props.amatLogo}></Header>
+                        {headerOnly ?
+                            null :
+                            <div className="MainHolder">
+                                <div className="OnDemandDash">
+                                    <p>Looking for the ON DEMAND Dashboard?</p>
+                                    <a href="/dashboard">Click here</a>
                                 </div>
-                                <div className="Mainwrapper Mainwrapper_dashboard">
-                                    <ManagerDashboard />
+                                <div className="MainHolderWrapper">
+                                    <div className="Big-Logo-Img">
+                                        <img src={this.props.amatLogo} alt="appliedx"/>
+                                    </div>
+                                    <div className="Mainwrapper Mainwrapper_dashboard">
+                                        <ManagerDashboard />
+                                    </div>
+                                    <Navcards items={navcards} navcardsImgs={this.props.navcardsImgs}></Navcards>
                                 </div>
-                                <Navcards items={navcards} navcardsImgs={this.props.navcardsImgs}></Navcards>
                             </div>
-                        </div>
-                        <Footer/>
+                        }
+                      </TeamProvider>
                     </UserProfileProvider>
                 </APIDataProvider>
             </div>
