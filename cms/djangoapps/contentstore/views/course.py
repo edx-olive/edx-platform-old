dfc396c33846c03e733454cf7c468d4e993140e3
Gábor Boros <gabor.brs@gmail.com>
2021-03-09 14:36:24 +0100

    Revert "Adding Grading Policy validation (#9)" and "Update translated LMS djangojs.js files (#16)"
    This reverts commit fe1bbebd and implicitly 9ab4499 as well.

--- cms/djangoapps/contentstore/views/course.py
+++ cms/djangoapps/contentstore/views/course.py
@@ -324,7 +324,7 @@ def course_search_index_handler(request, course_key_string):
         }), content_type=content_type, status=200)
 
 
-def _course_outline_json(request, course_module, graders=None):
+def _course_outline_json(request, course_module):
     """
     Returns a JSON representation of the course module and recursively all of its children.
     """
@@ -338,7 +338,6 @@ def _course_outline_json(request, course_module, graders=None):
         course_outline=False if is_concise else True,
         include_children_predicate=include_children_predicate,
         is_concise=is_concise,
-        graders=graders,
         user=request.user
     )
 
@@ -1137,11 +1136,10 @@ def grading_handler(request, course_key_string, grader_index=None):
     course_key = CourseKey.from_string(course_key_string)
     with modulestore().bulk_operations(course_key):
         course_module = get_course_and_check_access(course_key, request.user)
-        course_details = CourseGradingModel.fetch(course_key)
-        course_structure = _course_outline_json(request, course_module, graders=course_details.graders)
-        course_details.count_assignment_types(course_structure)
 
         if 'text/html' in request.META.get('HTTP_ACCEPT', '') and request.method == 'GET':
+            course_details = CourseGradingModel.fetch(course_key)
+
             return render_to_response('settings_graders.html', {
                 'context_course': course_module,
                 'course_locator': course_key,
@@ -1153,7 +1151,7 @@ def grading_handler(request, course_key_string, grader_index=None):
             if request.method == 'GET':
                 if grader_index is None:
                     return JsonResponse(
-                        course_details,
+                        CourseGradingModel.fetch(course_key),
                         # encoder serializes dates, old locations, and instances
                         encoder=CourseSettingsEncoder
                     )
@@ -1167,11 +1165,8 @@ def grading_handler(request, course_key_string, grader_index=None):
 
                 # None implies update the whole model (cutoffs, graceperiod, and graders) not a specific grader
                 if grader_index is None:
-                    course_details = CourseGradingModel.update_from_json(course_key, request.json, request.user)
-                    course_structure = _course_outline_json(request, course_module, graders=course_details.graders)
-                    course_details.count_assignment_types(course_structure)
                     return JsonResponse(
-                        course_details,
+                        CourseGradingModel.update_from_json(course_key, request.json, request.user),
                         encoder=CourseSettingsEncoder
                     )
                 else:
