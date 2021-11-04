e8f5cb284a9bd87f5cd53dbb59737ab3ca7effdd
Dmytro Nefyodov <dmytro.nefyodov@raccoongang.com>
2021-11-04 14:46:43 +0000

    feat: [CAMPUS-81] ported feature (improved signup form) to Koa

--- lms/djangoapps/learner_dashboard/tests/test_utils.py
+++ lms/djangoapps/learner_dashboard/tests/test_utils.py
@@ -5,9 +5,15 @@ Unit test module covering utils module
 
 import ddt
 import six
+import pytz
+from datetime import date, datetime
+
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 from lms.djangoapps.learner_dashboard import utils
+from openedx.core.djangoapps.site_configuration.tests.test_util import with_site_configuration
+from student.models import UserProfile
 
 
 @ddt.ddt
@@ -24,3 +30,58 @@ class TestUtils(TestCase):
         """
         actual = utils.strip_course_id(path + six.text_type(utils.FAKE_COURSE_KEY))
         self.assertEqual(actual, path)
+
+
+class DisclaimerIncompleteFieldsTestCase(TestCase):
+    """
+    Test cases to display or not the reminder alert if a user
+    has left some required fields in blank after n days.
+    """
+
+    def setUp(self):
+        self.user = User.objects.create(
+            username='my_self_user',
+            date_joined=date(2016, 3, 1)
+        )
+        self.user_profile = UserProfile.objects.create(
+            user_id=self.user.id,
+            year_of_birth=1989
+        )
+
+    @with_site_configuration(configuration={"DAYS_PASSED_TO_ALERT_PROFILE_INCOMPLETION": 5})
+    @with_site_configuration(configuration={"FIELDS_TO_CHECK_PROFILE_COMPLETION": ['gender', 'city', 'year_of_birth']})
+    def test_showing_alert(self):
+        """
+        Test the case when the alert should be displayed, due to the user created only having one
+        additional field filled.
+        """
+        utils.disclaimer_incomplete_fields_notification(self.user_profile)
+
+    @with_site_configuration(configuration={"DAYS_PASSED_TO_ALERT_PROFILE_INCOMPLETION": 10})
+    @with_site_configuration(configuration={"FIELDS_TO_CHECK_PROFILE_COMPLETION": ['year_of_birth']})
+    def test_not_showing_alert(self):
+        """
+        Test the case when the alert should not be displayed, due to in settings we are just
+        passing the field that is filled.
+        """
+        utils.disclaimer_incomplete_fields_notification(self.user_profile)
+
+    @with_site_configuration(configuration={"DAYS_PASSED_TO_ALERT_PROFILE_INCOMPLETION": 5})
+    @with_site_configuration(configuration={"FIELDS_TO_CHECK_PROFILE_COMPLETION": []})
+    def test_not_showing_alert_no_fields(self):
+        """
+        Test the case when the alert should be displayed, due to the user created only having one
+        additional field filled.
+        """
+        utils.disclaimer_incomplete_fields_notification(self.user_profile)
+
+    @with_site_configuration(configuration={"DAYS_PASSED_TO_ALERT_PROFILE_INCOMPLETION": 5})
+    @with_site_configuration(configuration={"FIELDS_TO_CHECK_PROFILE_COMPLETION": []})
+    def test_not_showing_alert_invalid_period(self):
+        """
+        Test the case when the alert should be displayed, due to the user created only having one
+        additional field filled.
+        """
+        current = datetime.now(pytz.utc)
+        User.objects.filter(username='my_self_user').update(date_joined=current)
+        utils.disclaimer_incomplete_fields_notification(self.user_profile)
