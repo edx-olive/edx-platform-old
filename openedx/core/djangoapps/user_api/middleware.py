4ab12253ae59741313aa6c1008e0b4f426a0ef27
Glib Glugovskiy <glib.glugovskiy@raccoongang.com>
2022-04-08 21:24:27 +0300

    feat: add maintanence page

--- openedx/core/djangoapps/user_api/middleware.py
+++ openedx/core/djangoapps/user_api/middleware.py
@@ -4,14 +4,32 @@ Adds user's tags to tracking event context.
 """
 
 from django.utils.deprecation import MiddlewareMixin
+from django.conf import settings
 
 from eventtracking import tracker
 from opaque_keys import InvalidKeyError
 from opaque_keys.edx.keys import CourseKey
 
 from common.djangoapps.track.contexts import COURSE_REGEX
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
 from .models import UserCourseTag
+
+
+def redirect_to_maintenance(absolute_url, path, user):
+    """Redirect users who not enrolled in a course to a maintenance page."""
+    from urllib.parse import quote
+    from django.shortcuts import redirect
+
+    if path == '/maintenancepage' or path == '/login' or path.startswith(('/user_api/', '/api/', '/notifier_api/', '/oauth2/access_token')):
+        return None
+
+    if not user.is_authenticated():
+        return redirect("/login" + "?next={url}".format(url=quote(absolute_url, safe='')))
+    else:
+        if user.is_superuser:
+            return None
+    return redirect('/maintenancepage')
 
 
 class UserTagsEventContextMiddleware(MiddlewareMixin):
@@ -51,6 +69,12 @@ class UserTagsEventContextMiddleware(MiddlewareMixin):
             self.CONTEXT_NAME,
             context
         )
+
+        if configuration_helpers.get_value(
+            'CAMPUS_RESTRICT_COURSE_ACCESS',
+            settings.FEATURES.get("CAMPUS_RESTRICT_COURSE_ACCESS")
+        ):
+            return redirect_to_maintenance(request.build_absolute_uri(), request.path, request.user)
 
     def process_response(self, request, response):  # pylint: disable=unused-argument
         """Exit the context if it exists."""
